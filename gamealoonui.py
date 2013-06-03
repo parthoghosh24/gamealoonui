4884d5e94eb83a1d4446f215b2da1f8c22af1d62
Partho <defjam24@gmail.com>
2013-06-03 21:59:36 +0530

    Lots of changes Temporary images added New controllers added Favicon added Logo added Default Favicon removed CSS updated Many JS frameworks added like Jquey cycle, modal, etc.

--- gamealoonui.py
+++ gamealoonui.py
@@ -2,23 +2,38 @@ import web
 import urllib2
 import json
 
-render = web.template.render('templates', base='main')
-create = web.template.render('templates',base='editmain')
+
+web.config.debug=False
+web.config.session_parameters['cookie_name'] = 'webpy_session_id'
+web.config.session_parameters['cookie_domain'] = None
+web.config.session_parameters['timeout'] = 86400, #24 * 60 * 60, # 24 hours   in seconds
+web.config.session_parameters['ignore_expiry'] = True
+web.config.session_parameters['ignore_change_ip'] = True
+web.config.session_parameters['secret_key'] = 'fLjUfxqXtfNoIldA0A0J'
+web.config.session_parameters['expired_message'] = 'Session expired'
+
 
 urls = (
         '/','Index',
+        '/login/','Login',
+        '/search/','SearchHome',
+        '/searchSite/','Search',
         '/platform/(.*)','Platform',
         '/article/(.*)/','Article',
         '/article/(.*)/(.*)','SingleArticle',
-        '/user/','User',
-        '/(.*)','SingleUser'
+        '/user/','User',        
+        '/(.*)','SingleUser'   
+             
         )
 
 app = web.application(urls, globals())
+session = web.session.Session(app,web.session.DiskStore('sessions'), {'count': 0})
+render = web.template.render('templates', base='main', globals={'session':session})
+create = web.template.render('templates',base='editmain',globals={'context':session})
 
 class Index:
     def GET(self):                        
-        url="http://192.168.0.102:9000/platform/all"
+        url="http://localhost:9000/platform/all"
         response = urllib2.urlopen(url)
         jsonData=json.load(response)        
         return render.index(jsonData)
@@ -28,7 +43,7 @@ class Platform:
     def GET(self, platform):
         if platform == 'all':            
             return web.redirect('/')
-        url="http://192.168.0.102:9000/platform/"+platform        
+        url="http://localhost:9000/platform/"+platform        
         response = urllib2.urlopen(url)
         jsonData=json.load(response)                
         return render.index(jsonData)   
@@ -38,31 +53,56 @@ class Article:
     def GET(self,category):
         if "create" == category:
             return create.editarticle()      
-        url="http://192.168.0.102:9000/articles/"+category+"/totalScore"
+        url="http://localhost:9000/articles/"+category+"/totalScore"
         response = urllib2.urlopen(url)
         jsonData=json.load(response)
         return render.article(jsonData)
     
 class SingleArticle:
     def GET(self, username, title):      
-        url="http://192.168.0.102:9000/article/"+username+"/"+title
+        url="http://localhost:9000/article/"+username+"/"+title
         response = urllib2.urlopen(url)
         article=json.load(response)
         return render.singlearticle(article)
     
 class User: 
     def GET(self):
-        url="http://192.168.0.102:9000/users"
+        url="http://localhost:9000/users"
         response = urllib2.urlopen(url)
         jsonData=json.load(response)
         return render.user(jsonData)        
     
 class SingleUser:
     def GET(self,username):
-        url="http://192.168.0.102:9000/user/"+username
+        url="http://localhost:9000/user/"+username
         response = urllib2.urlopen(url)
         user=json.load(response)
         return render.singleuser(user)
+    
+class Login:
+    def POST(self):
+        sFormData = web.input()        
+        url="http://localhost:9000/user/"+sFormData.username+"/"+sFormData.password
+        response = urllib2.urlopen(url)
+        user=json.load(response)
+        web.header('Content-Type', 'application/json')    
+        return json.dumps(user)                    
+       
+class Search:
+    
+    def GET(self):                        
+        query=web.input()            
+        url="http://localhost:9000/search/q="+query.searchbox        
+        response = urllib2.urlopen(url)        
+        responseQuery=json.load(response)        
+        return render.search(responseQuery)
+            
+class SearchHome:
+    
+    def GET(self):
+        responseQuery={}
+        responseQuery['queryResponse']=[]
+        return render.search(responseQuery)
     
 if __name__ == "__main__":
     
--- static/javascripts/gloon.js
+++ static/javascripts/gloon.js
@@ -1,8 +1,63 @@
-$(function() {  
-  $(".basic").click(function(e) {
-       $("#container").modal();
-       
-       return false;
-      });	
-  
+$(window).load(function() {
+	$(".slider").each(function() {
+		$(this).cycle({
+			fx : 'scrollUp, scrollDown, scrollLeft, scrollRight',
+			randomizeEffects : true,
+			speed : 400,
+			timeout : 5000,
+			delay : $(this).data('value')
+		});
+	});
 });
+
+$(function() {
+	$("#tabs").tabs();
+
+	$("#login").click(function(e) {
+
+		$(".signuplogin").modal();
+		return false;
+	});
+	$(".basic").click(function(e) {
+		$("#container").modal({
+			onShow : function() {
+				tinymce.init({
+					selector : "textarea#content",
+					theme : "modern",
+					width : 800,
+					height : 400,
+					plugins : ["advlist autolink link image lists charmap preview hr anchor pagebreak spellchecker", "searchreplace wordcount visualblocks visualchars insertdatetime media nonbreaking emoticons", "save contextmenu directionality emoticons template paste textcolor"],
+					toolbar1 : "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+					toolbar2 : "preview media | forecolor emoticons",
+					content_css : "css/content.css"
+				});
+			}
+		});
+
+		return false;
+	});
+
+	$('#loginForm').unbind('submit').submit(function(e) {
+		  $.post('/login/',$("#loginForm").serialize(),function(data){		  	
+		  	if(data.available)
+		  	{		  		
+		  		$.modal.close();
+		  		$("#userdetailsbox span").text(""+data.username+"");
+		  		$("#userdetailsbox span").wrap('<a href="#"></a>');
+		  		
+		  		
+		  	}
+		  	else
+		  	{		  		
+		  		$("#errormessage").css("display","block");
+		  	}
+		  },'json');
+		  
+		  return false;
+		});
+
+});
+
+
+
+
