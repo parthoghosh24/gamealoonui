fca1d52b46bbff6fe9e27343d722f60e2eff6055
Partho <defjam24@gmail.com>
2013-11-01 01:59:59 +0530

    templates path mentioned

--- gamealoonui.py
+++ gamealoonui.py
@@ -49,7 +49,7 @@ app = web.application(urls, globals())
 store=web.session.DiskStore(curPath+'/sessions')
 session = web.session.Session(app, store, initializer={'username':'Guest','userId':'Guest','loggedIn':False,'baseUrl':'http://162.144.38.150:9000','userAvatar':''})
 web.config.session_parameters.update(cookie_name="test_cookie", cookie_domain="/",cookie_path=store,timeout="60")    
-render = web.template.render('templates/', base='main', globals={'session':session})
+render = web.template.render(curPath+'/templates/', base='main', globals={'session':session})
 
 class Index:
     def GET(self):       
